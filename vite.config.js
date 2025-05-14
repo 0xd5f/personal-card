744230c12abe3f9708fc263031b7c0df1caa5daf
Alex <79174725+0xd5f@users.noreply.github.com>
2025-05-14 04:03:27 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/personal-card/",
+  base: '/personal-card/',
   plugins: [react()],
 })
